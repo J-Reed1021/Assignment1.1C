5b1e50e39eea2768467a93b1495bc2c57328ce03
joshu <joshu@REEDPC>
2024-05-16 14:44:35 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the first number");
+int fNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number");
+int sNumber = Convert.ToInt32(Console.ReadLine());
+
+int sum = fNumber + sNumber;
+Console.WriteLine("the sum is " +  sum);
